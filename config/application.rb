3342bf75daba2bced5dfff74d65a707a57c48fbc
Sawant Shah <sawant.shah@key4talent.com>
2016-11-27 17:46:28 +0100

    Move away from Ruby based schema, to SQL based schema.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Themoon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+	# Save database schema as sql instead of ruby
+	config.active_record.schema_format = :sql
   end
 end
