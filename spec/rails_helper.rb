6e4fc2df50e46d65a0efdfa1dc09bb0459070ed7
Sawant Shah <sawant.shah@key4talent.com>
2016-12-04 14:17:51 +0100

    Fix typo.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'rspec/rails'
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
-Capybara.default_drive = :poltergeist
+Capybara.default_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
